3a92e826dcca35a04f6d105d866ca560e1fc28d5
hienle15 <hienle88995@gmail.com>
2025-07-09 20:54:56 +0700

    keep original version

--- vite.config.js
+++ vite.config.js
@@ -12,8 +12,4 @@ export default defineConfig({
     host: '0.0.0.0',     // 👈 Cho phép truy cập từ IP trong mạng LAN
     port: 5173,          // 👈 Cổng mong muốn (nếu cần)
   },
-  preview: {
-    host: '0.0.0.0',  // 👈 BẮT BUỘC để Railway truy cập được
-    port: parseInt(process.env.PORT) || 4173, // Railway sẽ cung cấp biến PORT
-  }
 })
